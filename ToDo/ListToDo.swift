3cab364e4f85a28de9aeee30f64d14badffc004b
Nikhil Dhavale <nikhil_dhavale@hotmail.com>
2023-11-20 13:14:57 +0530

    Setting up project for watch , tv and phone

---
+++ ToDo/ListToDo.swift
@@ -0,0 +1,18 @@
+//
+//  ListToDo.swift
+//  ToDo
+//
+//  Created by Nikhil Dhavale on 20/11/23.
+//
+
+import SwiftUI
+
+struct ListToDo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListToDo()
+}
