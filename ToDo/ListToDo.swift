87f6775595ca7fe2e71206fcdf70d67a3e471b5c
Nikhil Dhavale <nikhil_dhavale@hotmail.com>
2023-11-20 13:15:22 +0530

    adding gitignore

--- ToDo/ListToDo.swift
+++ ToDo/ListToDo.swift
@@ -7,12 +7,11 @@
 
 import SwiftUI
 
-struct ListToDo: View {
+struct ListToDoView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List{
+            
+        }
     }
 }
 
-#Preview {
-    ListToDo()
-}
