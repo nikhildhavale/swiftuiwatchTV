505a82a3e3dd2959b9b8e27007ea36722087043c
Nikhil Dhavale <nikhil_dhavale@hotmail.com>
2023-11-20 12:02:16 +0530

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Nikhil Dhavale on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
