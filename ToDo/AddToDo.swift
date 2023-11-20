728e7657c6c0640aea4d31636a541bf6eb256b0e
Nikhil Dhavale <nikhil_dhavale@hotmail.com>
2023-11-20 15:31:59 +0530

    Added ui to add to list

---
+++ ToDo/AddToDo.swift
@@ -0,0 +1,18 @@
+//
+//  AddToDo.swift
+//  ToDo
+//
+//  Created by Nikhil Dhavale on 20/11/23.
+//
+
+import SwiftUI
+
+struct AddToDo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddToDo()
+}
